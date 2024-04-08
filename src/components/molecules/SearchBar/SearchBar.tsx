a7122a29b38771be8c92062c95a5a3a4fbe21eaf
Rafal <rafek131@wp.pl>
2024-04-08 17:40:09 +0200

    search input is now controlled input

--- src/components/molecules/SearchBar/SearchBar.tsx
+++ src/components/molecules/SearchBar/SearchBar.tsx
@@ -1,9 +1,14 @@
+import { FormEvent, useState } from 'react';
 import { ClearInputButton, SearchInput, Wrapper } from './SearchBar.styles';
 
 export const SearchBar = () => {
+	const [inputValue, setInputValue] = useState('');
+
+	const handleInputChange = (e: FormEvent<HTMLInputElement>) => setInputValue(e.currentTarget.value);
+
 	return (
 		<Wrapper>
-			<SearchInput placeholder='add new item' />
+			<SearchInput placeholder='add new item' value={inputValue} onChange={handleInputChange} />
 			<ClearInputButton aria-label='clear input' type='button'>
 				<img src='src/assets/icons/x-circle.svg' alt='' />
 			</ClearInputButton>
