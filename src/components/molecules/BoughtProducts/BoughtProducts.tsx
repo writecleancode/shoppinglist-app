35ff973c9b0e8e88d3a17d1b256d34bd5d2b750c
Rafal <rafek131@wp.pl>
2024-04-10 13:43:48 +0200

    deleted redundant code

--- src/components/molecules/BoughtProducts/BoughtProducts.tsx
+++ src/components/molecules/BoughtProducts/BoughtProducts.tsx
@@ -1,15 +1,5 @@
-import { CategoryIcon } from 'src/components/atoms/CategoryIcon/CategoryIcon';
-import { StatusButton } from 'src/components/atoms/StatusButton/StatusButton';
 import { ProductType } from 'src/views/MainView';
-import styled from 'styled-components';
 import { ProductListItem } from '../ProductListItem/ProductListItem';
-// import { ProductListItem } from '../ProductsToBuy/ProductsToBuy';
-
-// export const BoughtProductListItem = styled(ProductListItem)`
-// 	border-bottom: none;
-// 	color: ${({ theme }) => theme.colors.lightBlack};
-// 	filter: grayscale(80%);
-// `;
 
 type BoughtProductsProps = {
 	boughtProducts: ProductType[];
--- src/components/molecules/ProductsToBuy/ProductsToBuy.tsx
+++ src/components/molecules/ProductsToBuy/ProductsToBuy.tsx
@@ -1,22 +1,6 @@
 import { StyledList } from './ProductsToBuy.styles';
-import { StatusButton } from 'src/components/atoms/StatusButton/StatusButton';
-import { CategoryIcon } from 'src/components/atoms/CategoryIcon/CategoryIcon';
-import styled from 'styled-components';
 import { ProductType } from 'src/views/MainView';
 import { ProductListItem } from '../ProductListItem/ProductListItem';
-
-// export const ProductListItem = styled.li`
-// 	display: flex;
-// 	align-items: center;
-// 	padding: 0.8rem;
-// 	padding-left: 0;
-// 	border-bottom: 1px solid #ececec;
-
-// 	@media (min-width: 380px) {
-// 		padding-left: 0.8rem;
-// 		padding-right: 1.6rem;
-// 	}
-// `;
 
 type ProductsToBuyProps = {
 	productsToBuy: ProductType[];
