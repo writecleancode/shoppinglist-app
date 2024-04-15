975e8b767e16a2dba66f105ed8781c55c2333eb6
Rafal <rafek131@wp.pl>
2024-04-15 11:14:54 +0200

    deleted redundant code

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { MainView } from './MainView';
 import { Wrapper } from './Root.styles';
-import { products } from 'src/data/products';
 
 export const Root = () => {
 	const [isAdditemActive, setAdditemState] = useState(false);
