0287270b70c16c23d9f1ddb619a2828a4c1c0371
Rafal <rafek131@wp.pl>
2024-04-23 11:29:46 +0200

    added 'RemoveBoughtProductsButton' (temporarily it's not working)

--- src/components/atoms/Header/Header.styles.ts
+++ src/components/atoms/Header/Header.styles.ts
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.header`
+	position: relative;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	gap: 0.8rem;
 	padding: 0.8rem;
 	background-color: ${({ theme }) => theme.colors.primary};
+	overflow: hidden;
 
 	@media (min-width: 992px) {
 		padding: 1.2rem;
@@ -19,4 +21,33 @@ export const Icon = styled.img`
 
 export const H1 = styled.h1`
 	font-size: 2.4rem;
+`;
+
+export const EllipsisButton = styled.button`
+	position: absolute;
+	right: 0;
+	padding: 0.8rem;
+	border: none;
+	background-color: transparent;
+	-webkit-tap-highlight-color: transparent;
+`;
+
+export const RemoveBoughtProductsButton = styled.button<{ $isVisible: boolean }>`
+	position: absolute;
+	right: 3.4rem;
+	display: flex;
+	align-items: center;
+	gap: 0.8rem;
+	padding: 0.8rem 1.6rem;
+	background-color: #eee;
+	border: 3px solid #aaa;
+	border: none;
+	border-radius: 100px;
+	color: #f10a0a;
+	font-size: 1.5rem;
+	box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+	translate: ${({ $isVisible }) => ($isVisible ? '0 0' : 'calc(100% + 4rem) 0')};
+	translate: ${({ $isVisible }) => ($isVisible ? '0 0' : '0 145%')};
+	opacity: ${({ $isVisible }) => ($isVisible ? '1' : '0')};
+	transition: translate 0.2s ease-in-out, opacity 0.2s;
 `;
--- src/components/atoms/Header/Header.tsx
+++ src/components/atoms/Header/Header.tsx
@@ -1,10 +1,26 @@
-import { H1, Icon, Wrapper } from './Header.styles';
+import { useState } from 'react';
+import { EllipsisButton, H1, Icon, RemoveBoughtProductsButton, Wrapper } from './Header.styles';
 
 export const Header = () => {
+	const [isRemoveButtonVisible, setRemoveButtonState] = useState(false);
+
+	const handleRemoveButtonState = () => setRemoveButtonState(prevState => !prevState);
+
+	const handleRemoveBoughtProductsButton = () => {
+		setRemoveButtonState(false);
+	};
+
 	return (
 		<Wrapper>
-			<Icon src="src/assets/icons/cart.svg" alt="" />
+			<Icon src='src/assets/icons/cart.svg' alt='' />
 			<H1>ShoppingList</H1>
+			<EllipsisButton onClick={handleRemoveButtonState}>
+				<img src='src/assets/icons/ellipsis-vertical.svg' alt='' />
+			</EllipsisButton>
+			<RemoveBoughtProductsButton $isVisible={isRemoveButtonVisible} onClick={handleRemoveBoughtProductsButton}>
+				<img src='src/assets/icons/trash.svg' alt='' />
+				Remove bought products
+			</RemoveBoughtProductsButton>
 		</Wrapper>
 	);
 };
